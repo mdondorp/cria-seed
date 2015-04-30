e6fa5d0456f4a83cd50a54d6f73b02a810572354
michiel@eyequestion.nl <frxEtkZsaTQo8FbwkJpNDAMAfDf3bGpDvAgAUaVwDfujqNREFG>
2015-04-30 20:14:38 +0200

    removed dupes

--- deployment/server/mailTest.js
+++ deployment/server/mailTest.js
@@ -4,7 +4,6 @@
 
 var express = require('express'),
     bodyParser = require('body-parser'),
-    bodyParser = require('body-parser'),
     sys = require('sys'),
     exec = require('child_process').exec,
     app = express(),
--- deployment/server/server.js
+++ deployment/server/server.js
@@ -4,7 +4,6 @@
 
 var express = require('express'),
     bodyParser = require('body-parser'),
-    bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     sys = require('sys'),
     exec = require('child_process').exec,
